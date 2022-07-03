42dc71d38633becd91026a90d15a55cccc7361f2
Potsawat Thinkanwatthana <potsawattkw@outlook.com>
2022-07-03 19:58:10 +0700

    feat: Initial commit

---
+++ hello_lf.php
@@ -0,0 +1,71 @@
+<?php
+$foo = "hello";
+$bar = "world";
+
+echo 0, $foo, $bar, "\n";
+
+echo 1, $foo,
+$bar,
+"\n";
+
+echo 2, $foo,
+$bar,
+"\n"
+;
+
+
+echo "\n";
+
+
+echo 0 . $foo . $bar . "\n";
+
+echo 1 . $foo .
+$bar .
+"\n";
+
+echo 2 . $foo .
+$bar .
+"\n"
+;
+
+echo 3 . $foo
+. $bar
+. "\n";
+
+echo 4 . $foo
+. $bar
+. "\n"
+;
+
+
+echo "\n";
+
+
+print 0 . $foo . $bar . "\n";
+
+print 1 . $foo .
+$bar .
+"\n";
+
+print 2 . $foo .
+$bar .
+"\n"
+;
+
+print 3 . $foo
+. $bar
+. "\n";
+
+print 4 . $foo
+. $bar
+. "\n"
+;
+
+
+echo "\n";
+
+echo $foo . "\n" . $bar;
+
+echo $foo . "\r\n" . $bar;
+
+echo "\n";
